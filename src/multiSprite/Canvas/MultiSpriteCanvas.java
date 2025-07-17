b04fe94e48f9cd094a51af0f09d2e7f85403b87f
bdasuncion <bry_of_the_sky@yahoo.com>
2025-07-18 00:52:49 +0800

    Implement dragging of multi sprite image

--- src/multiSprite/Canvas/MultiSpriteCanvas.java
+++ src/multiSprite/Canvas/MultiSpriteCanvas.java
@@ -4,12 +4,16 @@ import java.awt.AWTEvent;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
 import java.awt.event.MouseWheelEvent;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.util.Vector;
 
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.event.MouseInputAdapter;
 
 import componentInterface.MultiSpriteFrameInterface;
 import componentInterface.UpdaterInterface;
@@ -22,16 +26,23 @@ public class MultiSpriteCanvas extends JPanel implements MultiSpriteFrameInterfa
     private int scale;
     private int baseX, baseY;
     private UpdaterInterface updaterInterface;
+    private Point dragStart;
+    
      public MultiSpriteCanvas() {
     	 super();
     	 
-    	 this.enableEvents(AWTEvent.MOUSE_EVENT_MASK|AWTEvent.MOUSE_WHEEL_EVENT_MASK);
+    	 //this.enableEvents(AWTEvent.MOUSE_EVENT_MASK|AWTEvent.MOUSE_WHEEL_EVENT_MASK);
+    	 this.enableEvents(AWTEvent.MOUSE_WHEEL_EVENT_MASK);
     	 this.setVisible(true);
     	 scale = 4;
     	 //baseX = 32;
     	 //baseY = 16;
     	 baseX = 16;
     	 baseY = 16;
+    	 
+    	 ShiftImage controls = new ShiftImage();
+ 		this.addMouseListener(controls);
+ 		this.addMouseMotionListener(controls);
      }
     
     public void paintComponent(Graphics g) {
@@ -43,6 +54,10 @@ public class MultiSpriteCanvas extends JPanel implements MultiSpriteFrameInterfa
  		Graphics2D g2D =(Graphics2D)g;
  		BufferedImage disp;
  		
+ 		g2D.scale(scale, scale);
+		
+		g2D.translate(baseX/scale, baseY/scale);
+		
  		Vector<SpriteFrame> spriteSet = spriteFrames.getSpriteFrames();
  		if (spriteSet.size() <= 0) {	
  			return;
@@ -57,21 +72,26 @@ public class MultiSpriteCanvas extends JPanel implements MultiSpriteFrameInterfa
  			int flipYOffset = 0, flipXOffset = 0, flipVertical = 1, flipHorizontal = 1;
  			
  			if (spriteFrame.isFlippedVertical()) {
- 				flipYOffset= spriteFrame.getImage().getHeight()*scale;
+ 				flipYOffset= spriteFrame.getImage().getHeight();
  				flipVertical = -1;
  			}
  			
  			if (spriteFrame.isFlippedHorizontal()) {
- 				flipXOffset = spriteFrame.getImage().getWidth()*scale;
+ 				flipXOffset = spriteFrame.getImage().getWidth();
  				flipHorizontal = -1;
  			}
  			
  			
  			disp = spriteFrame.getImage();
- 			g2D.drawImage(disp, baseX*scale + (spriteFrame.getXOffset()*scale) + flipXOffset, 
- 					baseY*scale + (spriteFrame.getYOffset()*scale) + flipYOffset, 
- 					flipHorizontal*spriteFrame.getImage().getWidth()*scale,
- 		 			flipVertical*spriteFrame.getImage().getHeight()*scale, this);
+ 			//g2D.drawImage(disp, baseX*scale + (spriteFrame.getXOffset()*scale) + flipXOffset, 
+ 			//		baseY*scale + (spriteFrame.getYOffset()*scale) + flipYOffset, 
+ 			//		flipHorizontal*spriteFrame.getImage().getWidth()*scale,
+ 		 	//		flipVertical*spriteFrame.getImage().getHeight()*scale, this);
+ 			
+ 			g2D.drawImage(disp, (spriteFrame.getXOffset()) + flipXOffset, 
+ 					(spriteFrame.getYOffset()) + flipYOffset, 
+ 					flipHorizontal*spriteFrame.getImage().getWidth(),
+ 		 			flipVertical*spriteFrame.getImage().getHeight(), this);
 
  		}
  		
@@ -116,5 +136,33 @@ public class MultiSpriteCanvas extends JPanel implements MultiSpriteFrameInterfa
 		this.setPreferredSize(new Dimension(this.getWidth(), this.getHeight()));
 		this.revalidate();
 		this.repaint();
+	}
+	
+	protected class ShiftImage extends MouseInputAdapter {
+		int xTileSetPostion;
+		int yTileSetPostion;
+		int snapToWidth = 16;
+		int snapToHeight = 16;
+		public void mousePressed(MouseEvent e) {
+			if(e.getID() == MouseEvent.MOUSE_PRESSED && e.getButton() == MouseEvent.BUTTON1) {
+				dragStart = e.getPoint();
+			}
+		}
+		
+		@Override
+    	public void mouseDragged(MouseEvent e) {
+			int dragX = e.getX();
+			int dragY = e.getY();
+			
+			if (SwingUtilities.isLeftMouseButton(e)) {
+				Point dragEnd = e.getPoint();
+                baseX += (dragEnd.x - dragStart.x);
+                baseY += (dragEnd.y - dragStart.y);
+                dragStart = dragEnd;
+                repaint();
+			}
+			
+			repaint();
+        }
 	}
 }
